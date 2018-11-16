dcec2935a3c12da398af6401503f38e8e4409e40
Tommy <info@flexforge.me>
2018-11-16 15:08:41 +0100

    Improved ConsistentlyQuotedIdentifierSqlCodeAnalysisRule

--- src/Dibix.Sdk/CodeAnalysis/Rules/ConsistentlyQuotedIdentifierSqlCodeAnalysisRule.cs
+++ src/Dibix.Sdk/CodeAnalysis/Rules/ConsistentlyQuotedIdentifierSqlCodeAnalysisRule.cs
@@ -11,15 +11,15 @@ namespace Dibix.Sdk.CodeAnalysis.Rules
 
     public sealed class ConsistentlyQuotedIdentifierSqlCodeAnalysisRuleVisitor : SqlCodeAnalysisRuleVisitor
     {
-        public override void Visit(SchemaObjectName name)
+        public override void Visit(MultiPartIdentifier node)
         {
-            bool allEqual = name.Identifiers
+            bool allEqual = node.Identifiers
                                 .Where(x => !SqlConstants.ReservedFunctionNames.Contains(x.Value))
                                 .Select(x => x.QuoteType)
                                 .Distinct()
                                 .Count() > 1;
             if (allEqual)
-                base.Fail(name, name.Dump());
+                base.Fail(node, node.Dump());
         }
     }
 }
