7b565b98dbc8a7756306ccb29cd3b9116eca23e7
Tommy <info@flexforge.me>
2018-11-19 13:13:22 +0100

    Suppress IMAGE/No PK for a few helpLine use cases

--- src/Dibix.Sdk/CodeAnalysis/Rules/HeapTableSqlCodeAnalysisRule.cs
+++ src/Dibix.Sdk/CodeAnalysis/Rules/HeapTableSqlCodeAnalysisRule.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.SqlServer.TransactSql.ScriptDom;
 
 namespace Dibix.Sdk.CodeAnalysis.Rules
@@ -11,8 +12,19 @@ namespace Dibix.Sdk.CodeAnalysis.Rules
 
     public sealed class HeapTableSqlCodeAnalysisRuleVisitor : SqlCodeAnalysisRuleVisitor
     {
+        private static readonly HashSet<string> Workarounds = new HashSet<string>
+        {
+            "hlwfactivityevents"
+          , "hlwfinstanceevents"
+          , "hlwfuserevents"
+        };
+
         public override void Visit(CreateTableStatement node)
         {
+            // Temporary suppression for HelplineData.dacpac
+            if (Workarounds.Contains(node.SchemaObjectName.BaseIdentifier.Value))
+                return;
+
             bool hasTableConstraint = node.Definition.TableConstraints.OfType<UniqueConstraintDefinition>().Any(x => x.IsPrimaryKey);
             bool hasColumnConstraint = node.Definition.ColumnDefinitions.Any(x => x.Constraints.OfType<UniqueConstraintDefinition>().Any(y => y.IsPrimaryKey));
             if (!hasTableConstraint && !hasColumnConstraint)
--- src/Dibix.Sdk/CodeAnalysis/Rules/ObsoleteDataTypeSqlCodeAnalysisRule.cs
+++ src/Dibix.Sdk/CodeAnalysis/Rules/ObsoleteDataTypeSqlCodeAnalysisRule.cs
@@ -16,11 +16,43 @@ namespace Dibix.Sdk.CodeAnalysis.Rules
             SqlDataTypeOption.NText,
             SqlDataTypeOption.Image
         };
+        private static readonly IDictionary<SqlDataTypeOption, HashSet<string>> Workarounds = new Dictionary<SqlDataTypeOption, HashSet<string>>
+        {
+            {
+                SqlDataTypeOption.Image, new HashSet<string>
+                {
+                    "hlsysadhocprocessdefinition"
+                  , "hlsysdocument"
+                  , "hlsyslicexport"
+                  , "hlsyslicimport"
+                  , "hlsysobjectmodelassembly"
+                  , "hlsyssearchresult"
+                  , "hlwfactivityproject"
+                  , "hlwfcompletedscope"
+                  , "hlwfworkflowdefinition"
+                }
+            }
+        };
+        private string _tableName;
+
+        public override void Visit(CreateTableStatement node)
+        {
+            this._tableName = node.SchemaObjectName.BaseIdentifier.Value;
+        }
 
         public override void Visit(SqlDataTypeReference node)
         {
-            if (ObsoleteDataTypes.Contains(node.SqlDataTypeOption))
-                base.Fail(node, node.SqlDataTypeOption.ToString().ToUpperInvariant());
+            if (!ObsoleteDataTypes.Contains(node.SqlDataTypeOption))
+                return;
+
+            if (this._tableName != null
+             && Workarounds.TryGetValue(node.SqlDataTypeOption, out HashSet<string> workarounds)
+             && workarounds.Contains(this._tableName))
+            {
+                return;
+            }
+
+            base.Fail(node, node.SqlDataTypeOption.ToString().ToUpperInvariant());
         }
     }
 }
