428384f2d35a97ca352eb76d1bae339ea3490a62
Tommy <info@flexforge.me>
2019-01-10 13:32:20 +0100

    Enforce dibix sources via project only

--- src/Dibix.Sdk/CodeGeneration/ISqlAccessorGenerator.cs
+++ src/Dibix.Sdk/CodeGeneration/ISqlAccessorGenerator.cs
@@ -6,7 +6,7 @@ namespace Dibix.Sdk.CodeGeneration
     {
         string Generate();
         ISqlAccessorGenerator AddSource(string projectName);
-        ISqlAccessorGenerator AddSource(Action<ISourceSelectionExpression> configuration);
+        //ISqlAccessorGenerator AddSource(Action<ISourceSelectionExpression> configuration);
         ISqlAccessorGenerator AddSource(string projectName, Action<ISourceSelectionExpression> configuration);
         ISqlAccessorGenerator SelectOutputWriter<TWriter>() where TWriter : IWriter, new();
         ISqlAccessorGenerator SelectOutputWriter<TWriter>(Action<IOutputConfigurationExpression> configuration) where TWriter : IWriter, new();
