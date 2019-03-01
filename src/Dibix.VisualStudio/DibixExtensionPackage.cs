9437b7fd9999bf853190029b9aa8b8cafcf964ea
Tommy <info@flexforge.me>
2019-03-01 15:37:24 +0100

    Initial Dibix.VisualStudio extension

---
+++ src/Dibix.VisualStudio/DibixExtensionPackage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace Dibix.VisualStudio
+{
+    [Guid("9B336B30-57D9-4F88-8C3A-6976CAE9C29A")]
+    [InstalledProductRegistration("#110", "#112", "1.0")]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class DibixExtensionPackage : AsyncPackage
+    {
+        #region Overrides
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+        #endregion
+    }
+}
