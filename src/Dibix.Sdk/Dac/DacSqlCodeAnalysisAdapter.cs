4d70a6cd31805cc65c2a4a1ad9d21cc6d4dac3a6
Tommy <info@flexforge.me>
2019-03-14 16:45:05 +0100

    Backward compatibility for older Dibix.Dac.Extensions.dll installations

--- src/Dibix.Sdk/Dac/DacSqlCodeAnalysisAdapter.cs
+++ src/Dibix.Sdk/Dac/DacSqlCodeAnalysisAdapter.cs
@@ -23,3 +23,19 @@ namespace Dibix.Sdk.Dac
         }
     }
 }
+
+// OBSOLETE! For old installations of Dibix.Dac.Extensions.dll
+namespace Dibix.Sdk.CodeAnalysis.Dac
+{
+    internal sealed class DacSqlCodeAnalysisAdapter
+    {
+        private readonly Sdk.Dac.DacSqlCodeAnalysisAdapter _inner;
+
+        public DacSqlCodeAnalysisAdapter()
+        {
+            this._inner = new Sdk.Dac.DacSqlCodeAnalysisAdapter();
+        }
+
+        public IList<SqlRuleProblem> Analyze(SqlRuleExecutionContext context) => this._inner.Analyze(context);
+    }
+}
