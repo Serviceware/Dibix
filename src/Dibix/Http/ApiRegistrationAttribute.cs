f2f9c9210eb2fed4eeed1e53cbd5027cc068bc29
Tommy <info@flexforge.me>
2019-07-15 18:21:00 +0200

    Added ApiRegistrationAttribute from Server.Common

---
+++ src/Dibix/Http/ApiRegistrationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dibix.Http
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public sealed class ApiRegistrationAttribute : Attribute
+    {
+        public string AreaName { get; }
+
+        public ApiRegistrationAttribute(string areaName)
+        {
+            this.AreaName = areaName;
+        }
+    }
+}
