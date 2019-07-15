fc14c3885be03be309a8ad5104858458ac31375d
Tommy <info@flexforge.me>
2019-07-15 15:07:49 +0200

    Execute(commandText, commandType)

--- src/Dibix/Access/DatabaseAccessorExtensions.Execute.cs
+++ src/Dibix/Access/DatabaseAccessorExtensions.Execute.cs
@@ -5,10 +5,17 @@ namespace Dibix
 {
     public static partial class DatabaseAccessorExtensions
     {
+        // MasterData (InsertGlobalOrganizationViewConfiguration)"
         public static int Execute(this IDatabaseAccessor accessor, string sql)
         {
             Guard.IsNotNull(accessor, nameof(accessor));
             return accessor.Execute(sql, CommandType.Text, EmptyParameters.Instance);
+        }
+        // TaskReminder (ValidateReminders)
+        public static int Execute(this IDatabaseAccessor accessor, string sql, CommandType commandType)
+        {
+            Guard.IsNotNull(accessor, nameof(accessor));
+            return accessor.Execute(sql, commandType, EmptyParameters.Instance);
         }
         // DataImport
         public static int Execute(this IDatabaseAccessor accessor, string sql, CommandType commandType, Action<IParameterBuilder> configureParameters)
