e878aec46c10849f21b3ee68cbd68989a4649f83
Tommy <info@flexforge.me>
2019-07-17 15:30:39 +0200

    Implemented HttpResponse abstractions

---
+++ src/Dibix/Http/HttpResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace Dibix.Http
+{
+    public class HttpResponse
+    {
+        public HttpStatusCode StatusCode { get; }
+
+        public HttpResponse(HttpStatusCode statusCode) => this.StatusCode = statusCode;
+
+        public virtual HttpResponseMessage CreateResponse(HttpRequestMessage request) => request.CreateResponse(this.StatusCode);
+    }
+}
