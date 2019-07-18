1c0921ce6aedec90426395d80ab392147b349661
Tommy <info@flexforge.me>
2019-07-18 13:53:09 +0200

    Implemented ReflectionHttpActionTarget for external methods

--- src/Dibix/Http/ReflectionHttpActionTarget.cs
+++ src/Dibix/Http/ReflectionHttpActionTarget.cs
@@ -24,8 +24,26 @@ namespace Dibix.Http
 
         public static IHttpActionTarget Create(string externalTarget)
         {
-            // TODO: DataImport.Business.ExternalController#ExternalAction,DataImport.Business.Implementation
-            throw new NotSupportedException();
+            // DataImport.Business.ExternalController#ExternalAction,DataImport.Business.Implementation
+            string[] targetParts = externalTarget.Split(',');
+            if (targetParts.Length != 2)
+                throw new InvalidOperationException($"Invalid action target format: {externalTarget}");
+
+            int typeNameIndex = targetParts[0].LastIndexOf('.');
+            if (typeNameIndex < 0)
+                throw new InvalidOperationException($"Invalid action target format: {externalTarget}");
+
+            string assemblyName = targetParts[1];
+            string typeName = targetParts[0].Substring(0, typeNameIndex);
+            string methodName = targetParts[0].Substring(typeNameIndex + 1);
+
+            string assemblyQualifiedTypeName = $"{typeName},{assemblyName}";
+            Type type = Type.GetType(assemblyQualifiedTypeName, true);
+            MethodInfo method = type.GetMethod(methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+            if (method == null)
+                throw new InvalidOperationException($"Could not find a public static method named '{methodName}' on {assemblyQualifiedTypeName}");
+
+            return new ReflectionHttpActionTarget(method);
         }
     }
 }
