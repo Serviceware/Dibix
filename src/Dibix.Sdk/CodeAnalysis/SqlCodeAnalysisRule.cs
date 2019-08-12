a49e8424898c463207ab6ec5e446ee7e3763b2d9
Tommy <info@flexforge.me>
2019-08-12 11:02:52 +0200

    Removed SqlCodeAnalysisRule in favor of SqlCodeAnalysisRule<TVisitor>

--- src/Dibix.Sdk/CodeAnalysis/SqlCodeAnalysisRule.cs
+++ src/Dibix.Sdk/CodeAnalysis/SqlCodeAnalysisRule.cs
@@ -5,7 +5,7 @@ using Microsoft.SqlServer.TransactSql.ScriptDom;
 
 namespace Dibix.Sdk.CodeAnalysis
 {
-    public abstract class SqlCodeAnalysisRule : ISqlCodeAnalysisRule
+    public abstract class SqlCodeAnalysisRule<TVisitor> : ISqlCodeAnalysisRule where TVisitor : SqlCodeAnalysisRuleVisitor, new()
     {
         public abstract int Id { get; }
         public abstract string ErrorMessage { get; }
@@ -26,24 +26,18 @@ namespace Dibix.Sdk.CodeAnalysis
             return this.Errors;
         }
 
-        protected abstract void Analyze(TSqlFragment scriptFragment);
+        private void Analyze(TSqlFragment scriptFragment)
+        {
+            TVisitor visitor = new TVisitor();
+            visitor.ErrorHandler = (fragment, line, column, args) => this.Fail(fragment ?? scriptFragment, line, column, args);
+            scriptFragment.Accept(visitor);
+        }
 
-        protected void Fail(TSqlFragment fragment, int line, int column, params object[] args)
+        private void Fail(TSqlFragment fragment, int line, int column, params object[] args)
         {
             string errorText = $"[{this.Id:d3}] {String.Format(this.ErrorMessage, args)}";
             SqlCodeAnalysisError problem = new SqlCodeAnalysisError(this.Id, errorText, fragment, line, column);
             this.Errors.Add(problem);
-        }
-    }
-
-
-    public abstract class SqlCodeAnalysisRule<TVisitor> : SqlCodeAnalysisRule, ISqlCodeAnalysisRule where TVisitor : SqlCodeAnalysisRuleVisitor, new()
-    {
-        protected override void Analyze(TSqlFragment scriptFragment)
-        {
-            TVisitor visitor = new TVisitor();
-            visitor.ErrorHandler = (fragment, line, column, args) => base.Fail(fragment ?? scriptFragment, line, column, args);
-            scriptFragment.Accept(visitor);
         }
     }
 }
