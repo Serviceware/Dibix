3b1628225b93c6e5aa3d5d3c47f9f599f194967a
Tommy <info@flexforge.me>
2019-09-03 11:08:40 +0200

    HttpParameterResolverTest.Compile_PropertySource

---
+++ tests/Dibix.Tests/HttpParameterResolverTest.Base.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Dibix.Http;
+using Xunit;
+
+namespace Dibix.Tests
+{
+    public partial class HttpParameterResolverTest
+    {
+        private static IHttpParameterResolutionMethod Compile() => Compile(x => { });
+        private static IHttpParameterResolutionMethod Compile(Action<HttpActionDefinition> actionConfiguration)
+        {
+            HttpApiRegistration registration = new HttpApiRegistration(actionConfiguration);
+            registration.Configure();
+            HttpActionDefinition action = registration.Controllers.Single().Actions.Single();
+            MethodInfo method = action.Target.Build();
+            IHttpParameterResolutionMethod result = HttpParameterResolver.Compile(action, method.GetParameters());
+            return result;
+        }
+
+        private sealed class HttpApiRegistration : HttpApiDescriptor
+        {
+            private readonly string _methodName = $"{DetermineTestName()}_Target";
+            private readonly Action<HttpActionDefinition> _actionConfiguration;
+
+            public HttpApiRegistration(Action<HttpActionDefinition> actionConfiguration) => this._actionConfiguration = actionConfiguration;
+
+            public override void Configure() => base.RegisterController(null, x => x.AddAction(ReflectionHttpActionTarget.Create(typeof(HttpParameterResolverTest), this._methodName), this._actionConfiguration));
+
+            private static string DetermineTestName()
+            {
+                var query = from frame in new StackTrace().GetFrames()
+                            let method = frame.GetMethod()
+                            where method.IsDefined(typeof(FactAttribute))
+                            select method.Name;
+                string methodName = query.FirstOrDefault();
+                if (methodName == null)
+                    throw new InvalidOperationException("Could not detect test name");
+
+                return methodName;
+            }
+        }
+
+        private sealed class HttpParameterSourceProvider : IHttpParameterSourceProvider
+        {
+            public Type GetInstanceType(HttpActionDefinition action) => typeof(HttpParameterSource);
+
+            public Expression GetInstanceValue(Type instanceType, ParameterExpression argumentsParameter, ParameterExpression dependencyProviderParameter) => Expression.New(typeof(HttpParameterSource));
+        }
+
+        private sealed class HttpParameterSource
+        {
+            public int LocaleId => 1033;
+        }
+    }
+}
--- tests/Dibix.Tests/HttpParameterResolverTest.cs
+++ tests/Dibix.Tests/HttpParameterResolverTest.cs
@@ -1,23 +1,17 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
 using Dibix.Http;
 using Moq;
 using Xunit;
 
 namespace Dibix.Tests
 {
-    public class HttpParameterResolverTest
+    public partial class HttpParameterResolverTest
     {
         [Fact]
         public void Compile_Default()
         {
-            HttpApiRegistration registration = new HttpApiRegistration();
-            registration.Configure();
-            HttpActionDefinition action = registration.Controllers.Single().Actions.Single();
-            MethodInfo method = action.Target.Build();
-            IHttpParameterResolutionMethod result = HttpParameterResolver.Compile(action, method.GetParameters());
+            IHttpParameterResolutionMethod result = Compile();
             Assert.Equal(@".Lambda #Lambda1<Dibix.Http.HttpParameterResolver+ResolveParameters>(
     System.Collections.Generic.IDictionary`2[System.String,System.Object] $arguments,
     Dibix.Http.IParameterDependencyResolver $dependencyResolver) {
@@ -43,13 +37,45 @@ namespace Dibix.Tests
             dependencyResolver.Verify(x => x.Resolve<IDatabaseAccessorFactory>(), Times.Once);
         }
 
+        [Fact]
+        public void Compile_PropertySource()
+        {
+            HttpParameterSourceProviderRegistry.Register<HttpParameterSourceProvider>("SESSION");
+            IHttpParameterResolutionMethod result = Compile(x => x.ResolveParameter("lcid", "SESSION", "LocaleId"));
+            Assert.Equal(@".Lambda #Lambda1<Dibix.Http.HttpParameterResolver+ResolveParameters>(
+    System.Collections.Generic.IDictionary`2[System.String,System.Object] $arguments,
+    Dibix.Http.IParameterDependencyResolver $dependencyResolver) {
+    .Block(
+        Dibix.IDatabaseAccessorFactory $databaseaccessorfactory,
+        Dibix.Tests.HttpParameterResolverTest+HttpParameterSource $session) {
+        $databaseaccessorfactory = .Call $dependencyResolver.Resolve();
+        $session = .New Dibix.Tests.HttpParameterResolverTest+HttpParameterSource();
+        .Call $arguments.Add(
+            ""databaseAccessorFactory"",
+            (System.Object)$databaseaccessorfactory);
+        .Call $arguments.Add(
+            ""lcid"",
+            (System.Object)$session.LocaleId)
+    }
+}", result.Source);
+            Assert.False(result.Parameters.Any());
+
+            IDictionary<string, object> arguments = new Dictionary<string, object>();
+            Mock<IParameterDependencyResolver> dependencyResolver = new Mock<IParameterDependencyResolver>(MockBehavior.Strict);
+            Mock<IDatabaseAccessorFactory> databaseAccessorFactory = new Mock<IDatabaseAccessorFactory>(MockBehavior.Strict);
+
+            dependencyResolver.Setup(x => x.Resolve<IDatabaseAccessorFactory>()).Returns(databaseAccessorFactory.Object);
+
+            result.PrepareParameters(arguments, dependencyResolver.Object);
+
+            Assert.Equal(2, arguments.Count);
+            Assert.Equal(databaseAccessorFactory.Object, arguments["databaseAccessorFactory"]);
+            Assert.Equal(1033, arguments["lcid"]);
+            dependencyResolver.Verify(x => x.Resolve<IDatabaseAccessorFactory>(), Times.Once);
+        }
+
         private static void Compile_Default_Target(IDatabaseAccessorFactory databaseAccessorFactory) { }
 
-        private sealed class HttpApiRegistration : HttpApiDescriptor
-        {
-            private readonly string _methodName = $"{new StackTrace().GetFrame(1).GetMethod().Name}_Target";
-
-            public override void Configure() => base.RegisterController(null, x => x.AddAction(ReflectionHttpActionTarget.Create(typeof(HttpParameterResolverTest), this._methodName), y => { }));
-        }
+        private static void Compile_PropertySource_Target(IDatabaseAccessorFactory databaseAccessorFactory, int lcid) { }
     }
 }
