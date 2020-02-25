aef92057edb054c218f6551fccae102e16c4a7cf
Tommy <info@flexforge.me>
2020-02-25 16:48:13 +0100

    Make CodeGenerationTask tests environment independent

--- tests/Dibix.Sdk.Tests/CodeGeneration/Tests/CodeGenerationTaskTests.cs
+++ tests/Dibix.Sdk.Tests/CodeGeneration/Tests/CodeGenerationTaskTests.cs
@@ -36,7 +36,7 @@ namespace Dibix.Sdk.Tests.CodeGeneration
         public void Inline_SinglePrimitiveResult_WithoutDeclaration_Error()
         {
             ExecuteTestAndExpectError(@"Tests\Syntax\dbx_tests_syntax_singleprimitiveresult_invaliddeclaration.sql", @"One or more errors occured during code generation:
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Tests\Syntax\dbx_tests_syntax_singleprimitiveresult_invaliddeclaration.sql(2,1) : error : There are missing return declarations for the output statements. Please mark the header of the statement with a line per output containting this hint: -- @Return <ClrTypeName>");
+Tests\Syntax\dbx_tests_syntax_singleprimitiveresult_invaliddeclaration.sql(2,1) : error : There are missing return declarations for the output statements. Please mark the header of the statement with a line per output containting this hint: -- @Return <ClrTypeName>");
         }
 
         [Fact]
@@ -63,28 +63,28 @@ D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Tests\Syntax\dbx_test
         public void Inline_SingleConcreteResult_WithUnknownResultContract_Error()
         {
             ExecuteTestAndExpectError(@"Tests\Syntax\dbx_tests_syntax_singleconcreteresult_unknownresultcontract.sql", @"One or more errors occured during code generation:
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Tests\Syntax\dbx_tests_syntax_singleconcreteresult_unknownresultcontract.sql(1,12) : error : Could not resolve contract 'X'");
+Tests\Syntax\dbx_tests_syntax_singleconcreteresult_unknownresultcontract.sql(1,12) : error : Could not resolve contract 'X'");
         }
 
         [Fact]
         public void Inline_SingleConcreteResult_WithUnknownResultContractAssembly_Error()
         {
             ExecuteTestAndExpectError(@"Tests\Syntax\dbx_tests_syntax_singleconcreteresult_unknownresultcontractassembly.sql", @"One or more errors occured during code generation:
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Tests\Syntax\dbx_tests_syntax_singleconcreteresult_unknownresultcontractassembly.sql(1,12) : error : Could not locate assembly: A");
+Tests\Syntax\dbx_tests_syntax_singleconcreteresult_unknownresultcontractassembly.sql(1,12) : error : Could not locate assembly: A");
         }
 
         [Fact]
         public void InvalidContractSchema_Error()
         {
             ExecuteTestAndExpectError(Enumerable.Empty<string>(), Enumerable.Repeat(@"Contracts\Invalid.json", 1), @"One or more errors occured during code generation:
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Contracts\Invalid.json(3,12) : error : [JSON] Value ""x"" is not defined in enum. (Invalid.A)
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Contracts\Invalid.json(3,12) : error : [JSON] String 'x' does not match regex pattern '^#[A-Za-z]([\w]+)?\??(\[\]|\*?)?$'. (Invalid.A)
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Contracts\Invalid.json(3,12) : error : [JSON] JSON does not match any schemas from 'anyOf'. (Invalid.A)
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Contracts\Invalid.json(3,12) : error : [JSON] Invalid type. Expected Object but got String. (Invalid.A)
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Contracts\Invalid.json(3,12) : error : [JSON] Invalid type. Expected Object but got String. (Invalid.A)
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Contracts\Invalid.json(3,12) : error : [JSON] JSON does not match any schemas from 'anyOf'. (Invalid.A)
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Contracts\Invalid.json(2,14) : error : [JSON] Invalid type. Expected Array but got Object. (Invalid)
-D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Contracts\Invalid.json(2,14) : error : [JSON] JSON does not match any schemas from 'anyOf'. (Invalid)");
+Contracts\Invalid.json(3,12) : error : [JSON] Value ""x"" is not defined in enum. (Invalid.A)
+Contracts\Invalid.json(3,12) : error : [JSON] String 'x' does not match regex pattern '^#[A-Za-z]([\w]+)?\??(\[\]|\*?)?$'. (Invalid.A)
+Contracts\Invalid.json(3,12) : error : [JSON] JSON does not match any schemas from 'anyOf'. (Invalid.A)
+Contracts\Invalid.json(3,12) : error : [JSON] Invalid type. Expected Object but got String. (Invalid.A)
+Contracts\Invalid.json(3,12) : error : [JSON] Invalid type. Expected Object but got String. (Invalid.A)
+Contracts\Invalid.json(3,12) : error : [JSON] JSON does not match any schemas from 'anyOf'. (Invalid.A)
+Contracts\Invalid.json(2,14) : error : [JSON] Invalid type. Expected Array but got Object. (Invalid)
+Contracts\Invalid.json(2,14) : error : [JSON] JSON does not match any schemas from 'anyOf'. (Invalid)");
         }
 
         private static void ExecuteTest(string source) => ExecuteTest(Enumerable.Repeat(source, 1), Enumerable.Empty<string>());
@@ -102,7 +102,7 @@ D:\Serviceware\Common\Dibix\tests\Dibix.Sdk.Tests.Database\Contracts\Invalid.jso
 
             taskInstance.SetupGet(x => x.BuildEngine).Returns(buildEngine.Object);
             buildEngine.Setup(x => x.LogErrorEvent(It.IsAny<BuildErrorEventArgs>()))
-                       .Callback((BuildErrorEventArgs e) => errors.Add(new CompilerError(e.File, e.LineNumber, e.ColumnNumber, e.Code, e.Message)));
+                       .Callback((BuildErrorEventArgs e) => errors.Add(new CompilerError(e.File.Substring(DatabaseProjectDirectory.Length + 1), e.LineNumber, e.ColumnNumber, e.Code, e.Message)));
             
             bool result = CodeGenerationTask.Execute
             (
