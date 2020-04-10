946df2a326cd8ce891d0b715c63a9a8a9c4423fc
Tommy <info@flexforge.me>
2020-04-10 15:27:12 +0200

    Lock MissingPrimaryKeySqlCodeAnalysisRule suppressions

--- src/Dibix.Sdk/CodeAnalysis/Rules/MissingPrimaryKeySqlCodeAnalysisRule.cs
+++ src/Dibix.Sdk/CodeAnalysis/Rules/MissingPrimaryKeySqlCodeAnalysisRule.cs
@@ -14,21 +14,22 @@ namespace Dibix.Sdk.CodeAnalysis.Rules
     {
         // helpLine suppressions
         // Adding a PK here would be very slow due to the size of the tables
-        private static readonly ICollection<string> Workarounds = new HashSet<string>
+        private static readonly IDictionary<string, string> Suppressions = new Dictionary<string, string>
         {
-            "hlwfactivityevents"
-          , "hlwfinstanceevents"
-          , "hlwfuserevents"
+            ["hlwfactivityevents"] = "72ba2dc4e62d308ba557ffc9cd634b7b"
+          , ["hlwfinstanceevents"] = "f0446a332d61e5557b635cc3090f9b89"
+          , ["hlwfuserevents"] = "41fa48d0ecec2be844f4c45a416f9615"
         };
 
         protected override void Visit(TableModel tableModel, SchemaObjectName tableName, TableDefinition tableDefinition)
         {
-            if (Workarounds.Contains(tableName.BaseIdentifier.Value))
+            string name = tableName.BaseIdentifier.Value;
+            if (Suppressions.TryGetValue(name, out string hash) && hash == base.Hash) 
                 return;
 
             bool hasPrimaryKey = base.Model.HasPrimaryKey(tableModel, tableName);
             if (!hasPrimaryKey)
-                base.Fail(tableDefinition, tableModel.TypeDisplayName, tableName.BaseIdentifier.Value);
+                base.Fail(tableDefinition, tableModel.TypeDisplayName, name);
         }
     }
 }
