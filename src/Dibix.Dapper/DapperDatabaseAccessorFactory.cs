3012174a6536f8251085e709d4fd2c2602e8b741
Tommy <info@flexforge.me>
2020-04-27 16:51:38 +0200

    Provide a simple default dapper implementation for IDatabaseAccessorFactory

---
+++ src/Dibix.Dapper/DapperDatabaseAccessorFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+
+namespace Dibix.Dapper
+{
+    public sealed class DapperDatabaseAccessorFactory : IDatabaseAccessorFactory
+    {
+        private readonly string _connectionString;
+
+        public DapperDatabaseAccessorFactory(string connectionString) => this._connectionString = connectionString;
+
+        public IDatabaseAccessor Create() => new DapperDatabaseAccessor(new SqlConnection(this._connectionString));
+    }
+}
