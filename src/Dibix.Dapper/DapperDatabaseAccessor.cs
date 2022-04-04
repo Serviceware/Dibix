0fad95bdc3a0f5bd65b7de57b0dd4e15f3d83f6a
Tommy <info@flexforge.me>
2022-04-04 08:59:07 +0200

    Improve RAISERROR WITH NOWAIT handling in DIbix.Testing [package]

--- src/Dibix.Dapper/DapperDatabaseAccessor.cs
+++ src/Dibix.Dapper/DapperDatabaseAccessor.cs
@@ -27,57 +27,57 @@ namespace Dibix.Dapper
         #region Overrides
         protected override int Execute(string commandText, CommandType commandType, int? commandTimeout, ParametersVisitor parameters)
         {
-            return base.Connection.Execute(commandText, PrepareParameters(parameters), this._transaction, commandTimeout, commandType);
+            return base.Connection.Execute(commandText, CollectParameters(parameters), this._transaction, commandTimeout, commandType);
         }
 
         protected override Task<int> ExecuteAsync(string commandText, CommandType commandType, int? commandTimeout, ParametersVisitor parameters, CancellationToken cancellationToken)
         {
-            CommandDefinition command = new CommandDefinition(commandText, PrepareParameters(parameters), this._transaction, commandTimeout, commandType, cancellationToken: cancellationToken);
+            CommandDefinition command = new CommandDefinition(commandText, CollectParameters(parameters), this._transaction, commandTimeout, commandType, cancellationToken: cancellationToken);
             return base.Connection.ExecuteAsync(command);
         }
 
         protected override IEnumerable<T> QueryMany<T>(string commandText, CommandType commandType, ParametersVisitor parameters)
         {
             DecoratedTypeMap.Adapt<T>();
-            return base.Connection.Query<T>(commandText, PrepareParameters(parameters), this._transaction, commandType: commandType);
+            return base.Connection.Query<T>(commandText, CollectParameters(parameters), this._transaction, commandType: commandType);
         }
 
         protected override Task<IEnumerable<T>> QueryManyAsync<T>(string commandText, CommandType commandType, ParametersVisitor parameters, bool buffered, CancellationToken cancellationToken)
         {
             DecoratedTypeMap.Adapt<T>();
             CommandFlags flags = buffered ? CommandFlags.Buffered : CommandFlags.None;
-            CommandDefinition command = new CommandDefinition(commandText, PrepareParameters(parameters), this._transaction, commandType: commandType, flags: flags, cancellationToken: cancellationToken);
+            CommandDefinition command = new CommandDefinition(commandText, CollectParameters(parameters), this._transaction, commandType: commandType, flags: flags, cancellationToken: cancellationToken);
             return base.Connection.QueryAsync<T>(command);
         }
 
         protected override IEnumerable<TReturn> QueryMany<TFirst, TSecond, TReturn>(string commandText, CommandType commandType, ParametersVisitor parameters, Func<TFirst, TSecond, TReturn> map, string splitOn)
         {
             DecoratedTypeMap.Adapt<TFirst, TSecond>();
-            return base.Connection.Query(commandText, map, PrepareParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
+            return base.Connection.Query(commandText, map, CollectParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
         }
 
         protected override IEnumerable<TReturn> QueryMany<TFirst, TSecond, TThird, TReturn>(string commandText, CommandType commandType, ParametersVisitor parameters, Func<TFirst, TSecond, TThird, TReturn> map, string splitOn)
         {
             DecoratedTypeMap.Adapt<TFirst, TSecond, TThird>();
-            return base.Connection.Query(commandText, map, PrepareParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
+            return base.Connection.Query(commandText, map, CollectParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
         }
 
         protected override IEnumerable<TReturn> QueryMany<TFirst, TSecond, TThird, TFourth, TReturn>(string commandText, CommandType commandType, ParametersVisitor parameters, Func<TFirst, TSecond, TThird, TFourth, TReturn> map, string splitOn)
         {
             DecoratedTypeMap.Adapt<TFirst, TSecond, TThird, TFourth>();
-            return base.Connection.Query(commandText, map, PrepareParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
+            return base.Connection.Query(commandText, map, CollectParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
         }
 
         protected override IEnumerable<TReturn> QueryMany<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(string commandText, CommandType commandType, ParametersVisitor parameters, Func<TFirst, TSecond, TThird, TFourth, TFifth, TReturn> map, string splitOn)
         {
             DecoratedTypeMap.Adapt<TFirst, TSecond, TThird, TFourth, TFifth>();
-            return base.Connection.Query(commandText, map, PrepareParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
+            return base.Connection.Query(commandText, map, CollectParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
         }
 
         protected override IEnumerable<TReturn> QueryMany<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn>(string commandText, CommandType commandType, ParametersVisitor parameters, Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn> map, string splitOn)
         {
             DecoratedTypeMap.Adapt<TFirst, TSecond, TThird, TFourth, TFifth, TSixth>();
-            return base.Connection.Query(commandText, map, PrepareParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
+            return base.Connection.Query(commandText, map, CollectParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
         }
 
         protected override IEnumerable<TReturn> QueryMany<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TEighth, TNinth, TReturn>(string commandText, CommandType commandType, ParametersVisitor parameters, Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TEighth, TNinth, TReturn> map, string splitOn)
@@ -85,7 +85,7 @@ namespace Dibix.Dapper
             DecoratedTypeMap.Adapt<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TEighth, TNinth>();
             Type[] types = { typeof(TFirst), typeof(TSecond), typeof(TThird), typeof(TFourth), typeof(TFifth), typeof(TSixth), typeof(TSeventh), typeof(TEighth), typeof(TNinth) };
             Func<object[], TReturn> mapWrapper = x => map((TFirst)x[0], (TSecond)x[1], (TThird)x[2], (TFourth)x[3], (TFifth)x[4], (TSixth)x[5], (TSeventh)x[6], (TEighth)x[7], (TNinth)x[8]);
-            return base.Connection.Query(commandText, types, mapWrapper, PrepareParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
+            return base.Connection.Query(commandText, types, mapWrapper, CollectParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
         }
 
         protected override IEnumerable<TReturn> QueryMany<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TEighth, TNinth, TTenth, TEleventh, TReturn>(string commandText, CommandType commandType, ParametersVisitor parameters, Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TEighth, TNinth, TTenth, TEleventh, TReturn> map, string splitOn)
@@ -93,50 +93,52 @@ namespace Dibix.Dapper
             DecoratedTypeMap.Adapt<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TEighth, TNinth, TTenth, TEleventh>();
             Type[] types = { typeof(TFirst), typeof(TSecond), typeof(TThird), typeof(TFourth), typeof(TFifth), typeof(TSixth), typeof(TSeventh), typeof(TEighth), typeof(TNinth), typeof(TTenth), typeof(TEleventh) };
             Func<object[], TReturn> mapWrapper = x => map((TFirst)x[0], (TSecond)x[1], (TThird)x[2], (TFourth)x[3], (TFifth)x[4], (TSixth)x[5], (TSeventh)x[6], (TEighth)x[7], (TNinth)x[8], (TTenth)x[9], (TEleventh)x[10]);
-            return base.Connection.Query(commandText, types, mapWrapper, PrepareParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
+            return base.Connection.Query(commandText, types, mapWrapper, CollectParameters(parameters), this._transaction, commandType: commandType, splitOn: splitOn);
         }
 
         protected override T QuerySingle<T>(string commandText, CommandType commandType, ParametersVisitor parameters)
         {
             DecoratedTypeMap.Adapt<T>();
-            return base.Connection.QuerySingle<T>(commandText, PrepareParameters(parameters), this._transaction, commandType: commandType);
+            return base.Connection.QuerySingle<T>(commandText, CollectParameters(parameters), this._transaction, commandType: commandType);
         }
 
         protected override Task<T> QuerySingleAsync<T>(string commandText, CommandType commandType, ParametersVisitor parameters, CancellationToken cancellationToken)
         {
             DecoratedTypeMap.Adapt<T>();
-            CommandDefinition command = new CommandDefinition(commandText, PrepareParameters(parameters), this._transaction, commandType: commandType, cancellationToken: cancellationToken);
+            CommandDefinition command = new CommandDefinition(commandText, CollectParameters(parameters), this._transaction, commandType: commandType, cancellationToken: cancellationToken);
             return base.Connection.QuerySingleAsync<T>(command);
         }
 
         protected override T QuerySingleOrDefault<T>(string commandText, CommandType commandType, ParametersVisitor parameters)
         {
             DecoratedTypeMap.Adapt<T>();
-            return base.Connection.QuerySingleOrDefault<T>(commandText, PrepareParameters(parameters), this._transaction, commandType: commandType);
+            return base.Connection.QuerySingleOrDefault<T>(commandText, CollectParameters(parameters), this._transaction, commandType: commandType);
         }
 
         protected override IMultipleResultReader QueryMultiple(string commandText, CommandType commandType, ParametersVisitor parameters)
         {
-            SqlMapper.GridReader reader = base.Connection.QueryMultiple(commandText, PrepareParameters(parameters), this._transaction, commandType: commandType);
+            SqlMapper.GridReader reader = base.Connection.QueryMultiple(commandText, CollectParameters(parameters), this._transaction, commandType: commandType);
             return new DapperGridResultReader(reader, commandText, commandType, parameters);
         }
 
         protected override async Task<IMultipleResultReader> QueryMultipleAsync(string commandText, CommandType commandType, ParametersVisitor parameters, CancellationToken cancellationToken)
         {
-            SqlMapper.GridReader reader = await base.Connection.QueryMultipleAsync(new CommandDefinition(commandText, PrepareParameters(parameters), this._transaction, commandTimeout: null, commandType, cancellationToken: cancellationToken));
+            SqlMapper.GridReader reader = await base.Connection.QueryMultipleAsync(new CommandDefinition(commandText, CollectParameters(parameters), this._transaction, commandTimeout: null, commandType, cancellationToken: cancellationToken));
             return new DapperGridResultReader(reader, commandText, commandType, parameters);
         }
         #endregion
 
-        #region Private Methods
-        private static object PrepareParameters(ParametersVisitor parametersVisitor)
+        #region Protected Methods
+        protected static object CollectParameters(ParametersVisitor parametersVisitor)
         {
             Guard.IsNotNull(parametersVisitor, nameof(parametersVisitor));
             DynamicParameters @params = new DynamicParameters();
             parametersVisitor.VisitInputParameters((name, dataType, value, isOutput, customInputType) => @params.Add(name: name, value: NormalizeParameterValue(value), dbType: NormalizeParameterDbType(dataType, customInputType), direction: isOutput ? ParameterDirection.Output : (ParameterDirection?)null));
             return new DynamicParametersWrapper(@params, parametersVisitor);
         }
+        #endregion
 
+        #region Private Methods
         private static object NormalizeParameterValue(object value)
         {
             if (value is StructuredType tvp)
--- src/Dibix.Testing/Data/DatabaseTestUtility.cs
+++ src/Dibix.Testing/Data/DatabaseTestUtility.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
-using Dibix.Dapper;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Dibix.Testing.Data
@@ -17,28 +20,50 @@ namespace Dibix.Testing.Data
         }
         public static IDatabaseAccessorFactory CreateDatabaseAccessorFactory<TConfiguration>(TConfiguration configuration) where TConfiguration : DatabaseConfigurationBase, new()
         {
-            return new DapperDatabaseAccessorFactory(configuration.Database.ConnectionString);
+            return new DapperDatabaseAccessorFactory(configuration.Database.ConnectionString, RaiseErrorWithNoWaitBehavior.ExecuteScalar);
+        }
+
+        // RAISERROR WITH NOWAIT is useful to receive immediate progress of a long running command.
+        // To make sure the messages are immediately received, there are two options:
+        // 1. SqlConnection.FireInfoMessageEventOnUserErrors
+        // => This approach is generic but comes with the big downside, that all errors are sent to the InfoMessage and no exception is thrown.
+        //    They can be rethrown in a way, but the fact, that only severe exceptions are going through, and the call stack is confusing, makes this a very dirty option.
+        // 2. Don't use SqlCommand.ExecuteNonQuery
+        // => This approach is not generic and requires the caller to actually never use ExecuteNonQuery, even if it makes sense.
+        //    It's still the best option, because it doesn't require custom exception handling like with option 1.
+        //    To ensure the caller doesn't use ExecuteNonQuery, we can provide a custom implementation that always replaces ExecuteNonQuery with ExecuteScalar.
+        private enum RaiseErrorWithNoWaitBehavior
+        {
+            None,
+            FireInfoMessageEventOnUserErrors,
+            ExecuteScalar
         }
 
         private sealed class DapperDatabaseAccessorFactory : IDatabaseAccessorFactory
         {
             private readonly string _connectionString;
+            private readonly RaiseErrorWithNoWaitBehavior _raiseErrorWithNoWaitBehavior;
 
-            public DapperDatabaseAccessorFactory(string connectionString) => this._connectionString = connectionString;
+            public DapperDatabaseAccessorFactory(string connectionString, RaiseErrorWithNoWaitBehavior raiseErrorWithNoWaitBehavior)
+            {
+                this._connectionString = connectionString;
+                this._raiseErrorWithNoWaitBehavior = raiseErrorWithNoWaitBehavior;
+            }
 
             public IDatabaseAccessor Create()
             {
-                SqlConnection connection = new SqlConnection(this._connectionString)
+                SqlConnection connection = new SqlConnection(this._connectionString);
+
+                if (this._raiseErrorWithNoWaitBehavior == RaiseErrorWithNoWaitBehavior.FireInfoMessageEventOnUserErrors)
                 {
-                    FireInfoMessageEventOnUserErrors = true // Important for tracing when using RAISERROR WITH NOWAIT
-                };
-                connection.InfoMessage += OnInfoMessage;
-                return new DapperDatabaseAccessor(connection);
+                    connection.FireInfoMessageEventOnUserErrors = true;
+                    connection.InfoMessage += OnInfoMessage;
+                }
+
+                return new DapperDatabaseAccessor(connection, this._raiseErrorWithNoWaitBehavior);
             }
 
-            // We are using RAISERROR WITH NOWAIT to track long running progress.
-            // This however requires the property FireInfoMessageEventOnUserErrors to be set to true.
-            // When this is true, errors will trigger an info message event aswell, without throwing an exception.
+            // When FireInfoMessageEventOnUserErrors is true, errors will trigger an info message event aswell, without throwing an exception.
             // To restore the original behavior for errors, we have to throw ourselves.
             private static void OnInfoMessage(object sender, SqlInfoMessageEventArgs e)
             {
@@ -54,6 +79,36 @@ namespace Dibix.Testing.Data
                 // Exceptions within the InfoMessage handler will generally be caught and traced.
                 // Unless they are of a severe exception type.
                 throw new AccessViolationException(exception.Message, exception);
+            }
+        }
+
+        private sealed class DapperDatabaseAccessor : Dapper.DapperDatabaseAccessor
+        {
+            private readonly RaiseErrorWithNoWaitBehavior _raiseErrorWithNoWaitBehavior;
+
+            public DapperDatabaseAccessor(DbConnection connection, RaiseErrorWithNoWaitBehavior raiseErrorWithNoWaitBehavior) : base(connection)
+            {
+                this._raiseErrorWithNoWaitBehavior = raiseErrorWithNoWaitBehavior;
+            }
+
+            // ExecuteNonQuery is optimized, and will not process any messages, so RAISERROR WITH NOWAIT will not work
+            // Here we override the underlying behavior, without the caller having to do it.
+            protected override int Execute(string commandText, CommandType commandType, int? commandTimeout, ParametersVisitor parameters)
+            {
+                if (this._raiseErrorWithNoWaitBehavior != RaiseErrorWithNoWaitBehavior.ExecuteScalar)
+                    return base.Execute(commandText, commandType, commandTimeout, parameters);
+
+                _ = base.Connection.ExecuteScalar(commandText, CollectParameters(parameters), transaction: null, commandTimeout, commandType);
+                return default;
+            }
+            protected override async Task<int> ExecuteAsync(string commandText, CommandType commandType, int? commandTimeout, ParametersVisitor parameters, CancellationToken cancellationToken)
+            {
+                if (this._raiseErrorWithNoWaitBehavior != RaiseErrorWithNoWaitBehavior.ExecuteScalar)
+                    return await base.ExecuteAsync(commandText, commandType, commandTimeout, parameters, cancellationToken);
+
+                CommandDefinition command = new CommandDefinition(commandText, CollectParameters(parameters), transaction: null, commandTimeout, commandType, cancellationToken: cancellationToken);
+                _ = await base.Connection.ExecuteScalarAsync(command).ConfigureAwait(false);
+                return default;
             }
         }
     }
