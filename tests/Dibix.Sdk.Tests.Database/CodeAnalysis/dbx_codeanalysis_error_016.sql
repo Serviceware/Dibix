7b4277bc432379741f48dff123fad1512130a1a7
Tommy <info@flexforge.me>
2022-05-17 10:00:07 +0200

    Improve code coverage

--- tests/Dibix.Sdk.Tests.Database/CodeAnalysis/dbx_codeanalysis_error_016.sql
+++ tests/Dibix.Sdk.Tests.Database/CodeAnalysis/dbx_codeanalysis_error_016.sql
@@ -1,6 +1,11 @@
 ﻿CREATE PROCEDURE [dbo].[dbx_codeanalysis_error_016]
 AS
-  DECLARE @a TEXT
-  DECLARE @b NTEXT
-  DECLARE @c IMAGE
-  DECLARE @d DATETIME2
+    CREATE TABLE [dbo].[dbx_codeanalysis_error_016_table]
+    (
+        [id] INT       NOT NULL
+      , [a]  TEXT      NOT NULL
+      , [b]  NTEXT     NOT NULL
+      , [c]  IMAGE     NOT NULL
+      , [d]  DATETIME2 NOT NULL
+      , CONSTRAINT [PK_dbx_codeanalysis_error_016_table] PRIMARY KEY ([id])
+    )
