9fd5e5e1ab34b498cbdf35491a5fdca67a471662
Tommy <info@flexforge.me>
2023-05-11 16:06:15 +0200

    Remove FireInfoMessageEventOnUserErrors usage for ServiceBrokerSubscriber which is no longer required

--- src/Dibix.Worker.Abstractions/ServiceBrokerSubscriber.cs
+++ src/Dibix.Worker.Abstractions/ServiceBrokerSubscriber.cs
@@ -4,7 +4,6 @@ using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Dapper;
@@ -85,49 +84,30 @@ namespace Dibix.Worker.Abstractions
         #region Private Methods
         private async Task<ICollection<TMessage>> ReceiveMessageBatch(CancellationToken cancellationToken)
         {
-            using (IWorkerScope scope = _scopeFactory.Create())
+            using IWorkerScope scope = _scopeFactory.Create();
+            
+            if (scope.Connection is SqlConnection sqlConnection)
             {
-                SqlException? sqlException = null;
-                if (scope.Connection is SqlConnection sqlConnection)
+                void OnInfoMessage(object _, SqlInfoMessageEventArgs e) => Logger.LogDebug($"[SQL] {e.Message}");
+                sqlConnection.InfoMessage += OnInfoMessage;
+            }
+
+            using DbCommand command = scope.Connection.CreateCommand();
+            command.CommandText = ReceiveProcedureName;
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandTimeout = CommandTimeout;
+            AddParameter(command, "timeout", DbType.Int32, ReceiveTimeout);
+
+            using (EnterCancellationScope(cancellationToken, command))
+            {
+                // Normally we would use ExecuteReaderAsync(cancellationToken) here,
+                // but our receive procedures are using RAISERROR WITH NOWAIT to report progress in realtime.
+                // The usage of NOWAIT however seems to block the cancellation.
+                // Therefore we have to use the sync method and cancel the command ourselves.
+                // See: https://stackoverflow.com/questions/24738417/canceling-sql-server-query-with-cancellationtoken/24834029#24834029
+                using (IDataReader reader = await Task.Run(command.ExecuteReader, cancellationToken).ConfigureAwait(false))
                 {
-                    // Important for tracing when using RAISERROR WITH NOWAIT
-                    // Caution:
-                    // This also means that errors will not throw an exception,
-                    // and will instead trigger an InfoMessage event aswell.
-                    // Therefore we will throw them ourselves.
-                    sqlConnection.FireInfoMessageEventOnUserErrors = true;
-                    sqlConnection.InfoMessage += (_, e) => OnInfoMessage(e, ref sqlException);
-                }
-
-                using (DbCommand command = scope.Connection.CreateCommand())
-                {
-                    command.CommandText = ReceiveProcedureName;
-                    command.CommandType = CommandType.StoredProcedure;
-                    command.CommandTimeout = CommandTimeout;
-                    AddParameter(command, "timeout", DbType.Int32, ReceiveTimeout);
-
-                    // Normally we would use ExecuteReaderAsync(cancellationToken) here,
-                    // but our receive procedures are using RAISERROR WITH NOWAIT to report progress in realtime.
-                    // The usage of NOWAIT however seems to block the cancellation.
-                    // Therefore we have to use the sync method and cancel the command ourselves.
-                    // See: https://stackoverflow.com/questions/24738417/canceling-sql-server-query-with-cancellationtoken/24834029#24834029
-                    using (EnterCancellationScope(cancellationToken, command))
-                    {
-                        DbDataReader ExecuteReader()
-                        {
-                            DbDataReader reader = command.ExecuteReader();
-                            if (sqlException != null)
-                                throw sqlException;
-
-                            return reader;
-                        }
-
-                        using (IDataReader reader = await Task.Run(ExecuteReader, cancellationToken).ConfigureAwait(false))
-                        {
-                            return reader.Parse<TMessage>().ToArray();
-                        }
-                    }
-
+                    return reader.Parse<TMessage>().ToArray();
                 }
             }
         }
@@ -143,17 +123,6 @@ namespace Dibix.Worker.Abstractions
             command?.Cancel(); // this method throws a SqlException => catch needed!
         }
 
-        private void OnInfoMessage(SqlInfoMessageEventArgs args, ref SqlException? sqlException)
-        {
-            bool isError = args.Errors.Cast<SqlError>().Aggregate(false, (current, sqlError) => current || sqlError.Class > 10);
-            if (!isError)
-            {
-                Logger.LogDebug(args.Message);
-                return;
-            }
-            sqlException = SqlExceptionFactory.Create(args.Errors);
-        }
-
         private static void AddParameter(IDbCommand command, string parameterName, DbType parameterType, object value)
         {
             IDbDataParameter param = command.CreateParameter();
@@ -161,29 +130,6 @@ namespace Dibix.Worker.Abstractions
             param.DbType = parameterType;
             param.Value = value;
             command.Parameters.Add(param);
-        }
-        #endregion
-
-        #region Nested Types
-        private static class SqlExceptionFactory
-        {
-            private static readonly Func<SqlErrorCollection, SqlException> CompiledMethod = CompileSqlExceptionFactory();
-
-            public static SqlException Create(SqlErrorCollection errors) => CompiledMethod(errors);
-
-            private static Func<SqlErrorCollection, SqlException> CompileSqlExceptionFactory()
-            {
-                // (SqlErrorCollection errorCollection) => 
-                ParameterExpression errorCollectionParameter = Expression.Parameter(typeof(SqlErrorCollection), "errorCollectionParameter");
-
-                // SqlException.CreateException(errorCollection, null);
-                Expression serverVersion = Expression.Constant(null, typeof(string));
-                Expression createExceptionCall = Expression.Call(typeof(SqlException), "CreateException", Type.EmptyTypes, errorCollectionParameter, serverVersion);
-
-                Expression<Func<SqlErrorCollection, SqlException>> lambda = Expression.Lambda<Func<SqlErrorCollection, SqlException>>(createExceptionCall, errorCollectionParameter);
-                Func<SqlErrorCollection, SqlException> compiled = lambda.Compile();
-                return compiled;
-            }
         }
         #endregion
     }
