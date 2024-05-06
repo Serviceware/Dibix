f8a0128f277e4d67678c3eabf4fbeb5b51163ea6
Tommy <info@flexforge.me>
2024-05-06 07:38:25 +0200

    Only generate accessor assembly if project contains no endpoints or explicitly set

--- src/Dibix.Sdk.CodeGeneration/Output/AccessorCodeArtifactGenerationUnit.cs
+++ src/Dibix.Sdk.CodeGeneration/Output/AccessorCodeArtifactGenerationUnit.cs
@@ -1,10 +1,11 @@
-﻿using Dibix.Sdk.Abstractions;
+﻿using System;
+using Dibix.Sdk.Abstractions;
 
 namespace Dibix.Sdk.CodeGeneration
 {
     internal sealed class AccessorCodeArtifactGenerationUnit : CodeArtifactGenerationUnit<AccessorCodeGenerator>
     {
-        public override bool ShouldGenerate(CodeGenerationModel model) => true;
+        public override bool ShouldGenerate(CodeGenerationModel model) => !String.IsNullOrEmpty(model.AccessorTargetFileName);
         protected override string GetOutputName(CodeGenerationModel model) => model.AccessorTargetFileName;
         protected override AccessorCodeGenerator CreateGenerator(CodeGenerationModel model, ISchemaRegistry schemaRegistry, ILogger logger) => new AccessorCodeGenerator(model, schemaRegistry, logger);
     }
