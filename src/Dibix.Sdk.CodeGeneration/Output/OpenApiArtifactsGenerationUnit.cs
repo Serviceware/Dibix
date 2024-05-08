e74d026dfbfef9c8eaf25b773fce901a5f823177
Tommy <info@flexforge.me>
2024-05-08 16:25:05 +0200

    Ensure stable OpenAPI output when using nullable

--- src/Dibix.Sdk.CodeGeneration/Output/OpenApiArtifactsGenerationUnit.cs
+++ src/Dibix.Sdk.CodeGeneration/Output/OpenApiArtifactsGenerationUnit.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using Dibix.Sdk.Abstractions;
 using Dibix.Sdk.CodeGeneration.OpenApi;
 using Microsoft.OpenApi;
 using Microsoft.OpenApi.Extensions;
 using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Writers;
 using Newtonsoft.Json.Linq;
 
 namespace Dibix.Sdk.CodeGeneration
@@ -29,7 +31,11 @@ namespace Dibix.Sdk.CodeGeneration
             string yamlFilePath = BuildOutputPath(model.OutputDirectory, model.DocumentationTargetName, "yml");
             using (Stream stream = File.Open(yamlFilePath, FileMode.Create))
             {
-                document.SerializeAsYaml(stream, OpenApiSpecVersion.OpenApi3_0);
+                using (FormattingStreamWriter streamWriter = new FormattingStreamWriter(stream, CultureInfo.InvariantCulture))
+                {
+                    OpenApiYamlWriter writer = new NullRespectingOpenApiYamlWriter(streamWriter);
+                    document.Serialize(writer, OpenApiSpecVersion.OpenApi3_0);
+                }
             }
 
             // Unfortunately the validation of the Microsoft SDK is not as thorough as the one on https://editor.swagger.io
@@ -45,5 +51,24 @@ namespace Dibix.Sdk.CodeGeneration
         }
 
         private static string BuildOutputPath(string targetDirectory, string areaName, string extension) => Path.GetFullPath(Path.Combine(targetDirectory, $"{areaName}.{extension}"));
+
+        private sealed class NullRespectingOpenApiYamlWriter : OpenApiYamlWriter
+        {
+            public NullRespectingOpenApiYamlWriter(FormattingStreamWriter streamWriter) : base(streamWriter) { }
+            public NullRespectingOpenApiYamlWriter(FormattingStreamWriter streamWriter, OpenApiWriterSettings settings) : base(streamWriter, settings) { }
+
+            public override void WriteNull()
+            {
+                // The base method says the following
+                //   YAML allows null value to be represented by either nothing or the word null.
+                //   We will write nothing here.
+                base.WriteNull();
+
+                // However, when reading nothing, it will be treated as an empty string ''
+                // This will then cause the default to be empty string the next time it's written to file.
+                // Therefore, we always write the null literal to ensure a stable output.
+                Writer.Write("null");
+            }
+        }
     }
 }
