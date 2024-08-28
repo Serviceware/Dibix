9e93a128da2e44f6f50c73d99169a6690a3b61ed
Tommy <info@flexforge.me>
2024-08-28 16:03:22 +0200

    Expose http host publicly in Start-Http-Host script

--- build/Start-Http-Host.ps1
+++ build/Start-Http-Host.ps1
@@ -10,4 +10,4 @@ $ErrorActionPreference = 'Stop'
 $currentDirectory = $PSScriptRoot
 $scriptPath       = Join-Path $currentDirectory 'Start-Host.ps1'
 
-& $scriptPath -AppName 'Dibix.Http.Host' -Configuration $Configuration -Arguments '--urls=https://localhost:7287;http://localhost:5128'
+& $scriptPath -AppName 'Dibix.Http.Host' -Configuration $Configuration -Arguments '--urls=https://+:7287;http://+:5128'
