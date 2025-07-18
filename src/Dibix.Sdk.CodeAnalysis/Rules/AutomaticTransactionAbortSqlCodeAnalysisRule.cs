4c2b1b2f43cc383c43c91fe9fad2f03fff6ef03f
Tommy <info@flexforge.me>
2025-07-18 15:01:30 +0200

    AutomaticTransactionAbortSqlCodeAnalysisRule

---
+++ src/Dibix.Sdk.CodeAnalysis/Rules/AutomaticTransactionAbortSqlCodeAnalysisRule.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+namespace Dibix.Sdk.CodeAnalysis.Rules
+{
+    [SqlCodeAnalysisRule(id: 43)]
+    public sealed class AutomaticTransactionAbortSqlCodeAnalysisRule : SqlCodeAnalysisRule
+    {
+        protected override string ErrorMessageTemplate => "SET XACT_ABORT ON when using multiple write statements";
+
+        public AutomaticTransactionAbortSqlCodeAnalysisRule(SqlCodeAnalysisContext context) : base(context) { }
+
+        public override void Visit(TSqlBatch node)
+        {
+            BatchVisitor visitor = new BatchVisitor();
+            node.Accept(visitor);
+
+            if (!visitor.Valid)
+                Fail(node);
+        }
+
+        private sealed class BatchVisitor : TSqlFragmentVisitor
+        {
+            private int _modificationStatementCount;
+            private bool _foundSetXactAbort;
+
+            public bool Valid => _modificationStatementCount < 2 || _foundSetXactAbort;
+
+            public override void ExplicitVisit(PredicateSetStatement node)
+            {
+                if (node.Options == SetOptions.XactAbort && node.IsOn)
+                    _foundSetXactAbort = true;
+
+                base.ExplicitVisit(node);
+            }
+
+            public override void Visit(DataModificationStatement node)
+            {
+                _modificationStatementCount++;
+                base.Visit(node);
+            }
+        }
+    }
+}
--- tests/Dibix.Sdk.Tests.Database/Tables/dbx_anothertable.sql
+++ tests/Dibix.Sdk.Tests.Database/Tables/dbx_anothertable.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[dbx_anothertable]
 (
-	[id] INT NOT NULL
+	[id]   INT           NOT NULL
+  , [name] NVARCHAR(100)     NULL
   , CONSTRAINT [PK_dbx_anothertable] PRIMARY KEY ([id])
 )
