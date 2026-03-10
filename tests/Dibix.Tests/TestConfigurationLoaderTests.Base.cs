0f5f00f76410d83fe4caa9f8a191fc50c6e71f0d
Tommy <info@flexforge.me>
2026-03-10 08:38:31 +0100

    Added tests for TestConfigurationLoader

---
+++ tests/Dibix.Tests/TestConfigurationLoaderTests.Base.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Dibix.Testing;
+using Microsoft.Extensions.Options;
+
+namespace Dibix.Tests
+{
+    public sealed partial class TestConfigurationLoaderTests : TestBase
+    {
+    }
+
+    public class TestConfiguration
+    {
+        [ValidateObjectMembers]
+        public virtual NestedTestConfiguration Nested { get; } = new NestedTestConfiguration();
+    }
+
+    public class NestedTestConfiguration
+    {
+        public virtual int? InitializedValue { get; set; }
+        [Required]
+        public virtual int? NotInitializedValue { get; set; }
+    }
+}
---
+++ tests/Dibix.Tests/TestConfigurationLoaderTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Dibix.Testing;
+using Dibix.Testing.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dibix.Tests
+{
+    [TestClass]
+    public sealed partial class TestConfigurationLoaderTests
+    {
+        [TestMethod]
+        public void Load_Lazy_AccessNotInitializedProperty_ThrowsDuringPropertyAccess()
+        {
+            TestConfiguration configuration = TestConfigurationLoader.Load<TestConfiguration>(TestContext);
+            Assert.IsNotNull(configuration);
+            InvalidOperationException exception = Assert.ThrowsExactly<InvalidOperationException>(() => configuration.Nested.NotInitializedValue);
+            Assert.AreEqual("Property not configured: Nested:NotInitializedValue", exception.Message);
+        }
+
+        [TestMethod]
+        public void Load_Lazy_AccessInitializedProperty_DoesNotThrow()
+        {
+            TestConfiguration configuration = TestConfigurationLoader.Load<TestConfiguration>(TestContext);
+            Assert.IsNotNull(configuration);
+            Assert.AreEqual(1, configuration.Nested.InitializedValue);
+        }
+
+        [TestMethod]
+        public void Load_DataAnnotations_IncludesNotInitializedProperty_ThrowsDuringLoad()
+        {
+            OptionsValidationException exception = Assert.ThrowsExactly<OptionsValidationException>(() => TestConfigurationLoader.Load<TestConfiguration>(TestContext, TestConfigurationValidationBehavior.DataAnnotations));
+            Assert.AreEqual("DataAnnotation validation failed for 'TestConfiguration.Nested' members: 'NotInitializedValue' with the error: 'The NotInitializedValue field is required.'.", exception.Message);
+        }
+    }
+}
