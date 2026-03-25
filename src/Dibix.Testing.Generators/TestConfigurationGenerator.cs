38283169f082b169efbe84f0d3f99a63dd59601a
Tommy <info@flexforge.me>
2026-03-25 16:49:28 +0100

    Make use of SyntaxValueProvider.ForAttributeWithMetadataName

--- src/Dibix.Testing.Generators/TestConfigurationGenerator.cs
+++ src/Dibix.Testing.Generators/TestConfigurationGenerator.cs
@@ -19,12 +19,12 @@ namespace Dibix.Testing.Generators
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
             IncrementalValueProvider<string?> rootNamespace = context.AnalyzerConfigOptionsProvider.Select((x, _) => x.GetRootNamespace());
-            IncrementalValueProvider<ImmutableArray<ClassDescriptor>> classes = context.SyntaxProvider
-                                                                                       .CreateSyntaxProvider(predicate: MatchSyntaxNode, transform: Collect)
-                                                                                       .Where(static x => x is not null)
-                                                                                       .Collect()!;
+            IncrementalValueProvider<ImmutableArray<MemberDescriptor>> classes = context.SyntaxProvider
+                                                                                        .ForAttributeWithMetadataName(LazyValidationAttributeName, predicate: MatchSyntaxNode, transform: Collect)
+                                                                                        .Where(static x => x is not null)
+                                                                                        .Collect()!;
 
-            IncrementalValueProvider<(ImmutableArray<ClassDescriptor> Left, string? Right)> input = classes.Combine(rootNamespace);
+            IncrementalValueProvider<(ImmutableArray<MemberDescriptor> Left, string? Right)> input = classes.Combine(rootNamespace);
 
             context.RegisterSourceOutput(input, (x, y) => GenerateCode(x, y.Left, y.Right));
             context.RegisterPostInitializationOutput(x => x.CollectEmbeddedSources(nameof(TestConfigurationGenerator)));
@@ -32,83 +32,55 @@ namespace Dibix.Testing.Generators
 
         private static bool MatchSyntaxNode(SyntaxNode node, CancellationToken cancellationToken)
         {
-            if (node is not ClassDeclarationSyntax @class)
+            if (node is not VariableDeclaratorSyntax variableDeclarator)
                 return false;
 
-            foreach (MemberDeclarationSyntax member in @class.Members)
-            {
-                if (!member.IsKind(SyntaxKind.FieldDeclaration))
-                    continue;
+            if (variableDeclarator.Parent is not VariableDeclarationSyntax variableDeclaration)
+                return false;
 
-                if (!member.IsDefined("LazyValidation"))
-                    continue;
+            if (variableDeclaration.Parent is not FieldDeclarationSyntax fieldDeclaration)
+                return false;
 
-                return true;
-            }
+            if (fieldDeclaration.Parent is not ClassDeclarationSyntax)
+                return false;
 
-            return false;
+            return true;
         }
 
-        private static ClassDescriptor? Collect(GeneratorSyntaxContext context, CancellationToken cancellationToken)
+        private static MemberDescriptor Collect(GeneratorAttributeSyntaxContext context, CancellationToken cancellationToken)
         {
-            ClassDeclarationSyntax @class = (ClassDeclarationSyntax)context.Node;
-            INamedTypeSymbol? classSymbol = context.SemanticModel.GetDeclaredSymbol(@class);
+            VariableDeclaratorSyntax variableDeclarator = (VariableDeclaratorSyntax)context.TargetNode;
+            IFieldSymbol fieldSymbol = (IFieldSymbol)context.SemanticModel.GetDeclaredSymbol(variableDeclarator)!;
 
-            if (classSymbol == null)
-                return null;
-
-            ImmutableArray<MemberDescriptor>.Builder membersBuilder = ImmutableArray.CreateBuilder<MemberDescriptor>();
-
-            foreach (MemberDeclarationSyntax member in @class.Members)
+            bool isPrimitive = IsPrimitiveType(fieldSymbol.Type);
+            SymbolDisplayFormat format;
+            if (isPrimitive)
             {
-                if (member is not FieldDeclarationSyntax field)
-                    continue;
-
-                foreach (VariableDeclaratorSyntax variable in field.Declaration.Variables)
-                {
-                    if (context.SemanticModel.GetDeclaredSymbol(variable) is not IFieldSymbol fieldSymbol)
-                        continue;
-
-                    AttributeData? lazyValidationAttribute = fieldSymbol.GetAttributes().FirstOrDefault(x => x.AttributeClass?.ToDisplayString() == LazyValidationAttributeName);
-
-                    if (lazyValidationAttribute is null)
-                        continue;
-
-                    bool isPrimitive = IsPrimitiveType(fieldSymbol.Type);
-                    SymbolDisplayFormat format;
-                    if (isPrimitive)
-                    {
-                        format = SymbolDisplayFormat.CSharpErrorMessageFormat;
-                    }
-                    else
-                    {
-                        format = new SymbolDisplayFormat
-                        (
-                            globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Included
-                          , typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces
-                          , genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters
-                          , miscellaneousOptions: SymbolDisplayMiscellaneousOptions.None
-                        );
-                    }
-
-                    membersBuilder.Add(new MemberDescriptor
-                    (
-                        PropertyName: GeneratePropertyName(fieldSymbol.Name),
-                        FieldName: fieldSymbol.Name,
-                        TypeName: fieldSymbol.Type.ToDisplayString(format),
-                        IsPrimitive: isPrimitive
-                    ));
-                }
+                format = SymbolDisplayFormat.CSharpErrorMessageFormat;
+            }
+            else
+            {
+                format = new SymbolDisplayFormat
+                (
+                    globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Included
+                  , typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces
+                  , genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters
+                  , miscellaneousOptions: SymbolDisplayMiscellaneousOptions.None
+                );
             }
 
-            ImmutableArray<MemberDescriptor> endpoints = membersBuilder.ToImmutable();
-            string @namespace = classSymbol.ContainingNamespace?.ToDisplayString() ?? "Global";
-            string className = classSymbol.Name;
-            bool isSealed = @class.Modifiers.Any(x => x.IsKind(SyntaxKind.SealedKeyword));
-            bool isAbstract = @class.Modifiers.Any(x => x.IsKind(SyntaxKind.AbstractKeyword));
-            ClassDescriptor group = new ClassDescriptor(@namespace, className, endpoints, isSealed, isAbstract);
-
-            return group;
+            VariableDeclarationSyntax variableDeclaration = (VariableDeclarationSyntax)variableDeclarator.Parent!;
+            FieldDeclarationSyntax fieldDeclaration = (FieldDeclarationSyntax)variableDeclaration.Parent!;
+            ClassDeclarationSyntax classDeclaration = (ClassDeclarationSyntax)fieldDeclaration.Parent!;
+            MemberDescriptor member = new MemberDescriptor
+            (
+                PropertyName: GeneratePropertyName(fieldSymbol.Name),
+                FieldName: fieldSymbol.Name,
+                TypeName: fieldSymbol.Type.ToDisplayString(format),
+                IsPrimitive: isPrimitive,
+                ClassFactory: new ClassDescriptorFactory(context, classDeclaration)
+            );
+            return member;
         }
 
         private static bool IsPrimitiveType(ITypeSymbol type)
@@ -136,15 +108,16 @@ namespace Dibix.Testing.Generators
             return propertyName;
         }
 
-        private static void GenerateCode(SourceProductionContext context, ImmutableArray<ClassDescriptor> classes, string? rootNamespace)
+        private static void GenerateCode(SourceProductionContext context, ImmutableArray<MemberDescriptor> members, string? rootNamespace)
         {
-            foreach (ClassDescriptor @class in classes)
+            foreach (IGrouping<ClassDescriptorFactory, MemberDescriptor>? memberGroup in members.GroupBy(x => x.ClassFactory))
             {
-                context.AddSource($"{@class.ClassName}.g.cs", GenerateSharedTestClass(@class));
+                ClassDescriptor @class = memberGroup.Key.ClassAccessor.Value;
+                context.AddSource($"{@class.ClassName}.g.cs", GenerateSharedTestClass(@class, memberGroup.ToArray()));
             }
         }
 
-        private static string GenerateSharedTestClass(ClassDescriptor @class)
+        private static string GenerateSharedTestClass(ClassDescriptor @class, IReadOnlyList<MemberDescriptor> members)
         {
             return $$"""
                      {{SourceGeneratorUtility.GeneratedCodeHeader}}
@@ -152,7 +125,7 @@ namespace Dibix.Testing.Generators
                      namespace {{@class.Namespace}}
                      {
                          public{{(@class.IsSealed ? " sealed" : null)}}{{(@class.IsAbstract ? " abstract" : null)}} partial class {{@class.ClassName}} : global::Dibix.Testing.ConfigurationInitializationTracker
-                         {{{GenerateProperties(@class.Members)}}
+                         {{{GenerateProperties(members)}}
                          }
                      }
                      """;
@@ -205,8 +178,48 @@ namespace Dibix.Testing.Generators
             return property;
         }
 
-        private sealed record ClassDescriptor(string Namespace, string ClassName, IReadOnlyList<MemberDescriptor> Members, bool IsSealed, bool IsAbstract);
+        private sealed record ClassDescriptor(string Namespace, string ClassName, bool IsSealed, bool IsAbstract);
 
-        private sealed record MemberDescriptor(string PropertyName, string FieldName, string TypeName, bool IsPrimitive);
+        private sealed record MemberDescriptor(string PropertyName, string FieldName, string TypeName, bool IsPrimitive, ClassDescriptorFactory ClassFactory);
+
+        private sealed class ClassDescriptorFactory
+        {
+            private readonly GeneratorAttributeSyntaxContext _context;
+
+            public ClassDeclarationSyntax ClassDeclaration { get; }
+            public Lazy<ClassDescriptor> ClassAccessor { get; }
+
+            public ClassDescriptorFactory(GeneratorAttributeSyntaxContext context, ClassDeclarationSyntax classDeclaration)
+            {
+                _context = context;
+                ClassDeclaration = classDeclaration;
+                ClassAccessor = new Lazy<ClassDescriptor>(CreateClassDescriptor);
+            }
+
+            public override bool Equals(object? obj) => ReferenceEquals(this, obj) || obj is ClassDescriptorFactory other && Equals(other);
+
+            public override int GetHashCode() => ClassDeclaration.GetHashCode();
+
+            private ClassDescriptor CreateClassDescriptor()
+            {
+                INamedTypeSymbol classSymbol = _context.SemanticModel.GetDeclaredSymbol(ClassDeclaration)!;
+                string @namespace = classSymbol.ContainingNamespace?.ToDisplayString() ?? "Global";
+                string className = classSymbol.Name;
+                bool isSealed = ClassDeclaration.Modifiers.Any(x => x.IsKind(SyntaxKind.SealedKeyword));
+                bool isAbstract = ClassDeclaration.Modifiers.Any(x => x.IsKind(SyntaxKind.AbstractKeyword));
+                return new ClassDescriptor(@namespace, className, isSealed, isAbstract);
+            }
+
+            private bool Equals(ClassDescriptorFactory? other)
+            {
+                if (other is null)
+                    return false;
+
+                if (ReferenceEquals(this, other))
+                    return true;
+
+                return ClassDeclaration.Equals(other.ClassDeclaration);
+            }
+        }
     }
 }
