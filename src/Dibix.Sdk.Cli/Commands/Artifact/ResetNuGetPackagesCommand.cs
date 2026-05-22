167e191faea72504f42db53063fb63c7df61d827
Tommy <info@flexforge.me>
2026-05-22 10:04:25 +0200

    Add option to skip reverting consumer package version in "dibix artifact reset" CLI command

--- src/Dibix.Sdk.Cli/Commands/Artifact/ResetNuGetPackagesCommand.cs
+++ src/Dibix.Sdk.Cli/Commands/Artifact/ResetNuGetPackagesCommand.cs
@@ -7,10 +7,19 @@ namespace Dibix.Sdk.Cli
 {
     internal sealed class ResetNuGetPackagesCommand : ConsumerPackageCommand
     {
+        private readonly Option<bool> _revertConsumerPackageVersionOption;
+
         protected override string PackageNameArgumentDescription => "The name of the package to reset. If not specified, all packages will be reset.";
 
         public ResetNuGetPackagesCommand(EnvironmentVariableOption consumerDirectoryOption) : base("reset", "Removes the current version of the Dibix packages from the local NuGet package cache and reverts the consumer to the previous version.", consumerDirectoryOption)
         {
+            _revertConsumerPackageVersionOption = new Option<bool>("--revert-consumer-package-version", "-r")
+            {
+                Description = "Revert the consumer package version to the previous version.",
+                DefaultValueFactory = _ => true
+            };
+
+            Add(_revertConsumerPackageVersionOption);
         }
 
         protected override async Task<int> Execute(ParseResult parseResult, CancellationToken cancellationToken)
@@ -21,6 +30,7 @@ namespace Dibix.Sdk.Cli
             ConsoleUtility.WriteLineInformation(PackageName == null ? "Resetting all packages.." : $"Resetting only package '{PackageName}'");
 
             string[] packagesToReset = PackageName != null ? [PackageName] : ArtifactUtility.NuGetPackageNames;
+            bool revertConsumerPackageVersion = parseResult.GetRequiredValue<bool>(_revertConsumerPackageVersionOption);
 
             foreach (string packageName in packagesToReset)
             {
@@ -29,8 +39,11 @@ namespace Dibix.Sdk.Cli
                 ConsoleUtility.WriteLineDebug($"Removing package '{packageName}' version '{packageVersion}' from local NuGet package cache");
                 ArtifactUtility.RemovePackageFromNuGetPackageCache(packageName, packageVersion);
 
-                ConsoleUtility.WriteLineDebug($"Reverting consumer package reference of package '{packageName}'");
-                await ConsumerPackageManager.RevertPackageVersionChanges(packageName, ArtifactUtility.IsSdk(packageName), cancellationToken).ConfigureAwait(false);
+                if (revertConsumerPackageVersion)
+                {
+                    ConsoleUtility.WriteLineDebug($"Reverting consumer package reference of package '{packageName}'");
+                    await ConsumerPackageManager.RevertPackageVersionChanges(packageName, ArtifactUtility.IsSdk(packageName), cancellationToken).ConfigureAwait(false);
+                }
             }
 
             return 0;
